88bee4c0cd85dc0c9c455144135922a56f9deaec
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-10-27 11:27:22 +0300

    GetCity

---
+++ ResourceManagement/Controllers/CitiesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResourceManagement.Controllers
+{
+    public class CitiesController : Controller
+    {
+        public void GetCity(int cityId)
+        {
+
+        }
+    }
+}
--- ResourceManagement/Startup.cs
+++ ResourceManagement/Startup.cs
@@ -15,6 +15,7 @@ namespace ResourceManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,16 +30,18 @@ namespace ResourceManagement
                 app.UseExceptionHandler();
             }
 
-            app.Run((context) =>
-                        {
-                            throw new Exception("asd");
-                        }
-            );
+            app.UseMvc();
 
-            //app.Run(async (context) =>
-            //{
-            //    await context.Response.WriteAsync("Hello World!");
-            //});
+            //app.Run((context) =>
+            //            {
+            //                throw new Exception("asd");
+            //            }
+            //);
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
         }
     }
 }
