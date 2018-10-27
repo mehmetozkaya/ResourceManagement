9d016e6e009f450bf958471c93885fcb3b9d8e28
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-10-27 20:02:12 +0300

    GetCities

--- ResourceManagement/Controllers/CitiesController.cs
+++ ResourceManagement/Controllers/CitiesController.cs
@@ -12,5 +12,15 @@ namespace ResourceManagement.Controllers
         {
 
         }
+
+        [HttpGet("api/cities")]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(new List<object>
+            {
+                new { id = 1, Name = "Istanbul" },
+                new { id = 2, Name = "Antep" }
+            });
+        }
     }
 }
