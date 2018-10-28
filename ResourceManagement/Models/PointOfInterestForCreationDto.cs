6a467bca88a18f8e520dbaaeaffc66ebd58a21fb
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-10-28 21:43:43 +0300

    PointOfInterestForCreationDto

---
+++ ResourceManagement/Models/PointOfInterestForCreationDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResourceManagement.Models
+{
+    public class PointOfInterestForCreationDto
+    {
+        [Required(ErrorMessage = "You should provide a name value.")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [MaxLength(200)]
+        public string Description { get; set; }
+    }
+}
