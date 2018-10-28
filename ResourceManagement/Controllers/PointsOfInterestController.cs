1f23280281a61c208e53e085c95666ddfc16bef6
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-10-28 23:52:50 +0300

    UpdatePointOfInterest

--- ResourceManagement/Controllers/PointsOfInterestController.cs
+++ ResourceManagement/Controllers/PointsOfInterestController.cs
@@ -77,7 +77,40 @@ namespace ResourceManagement.Controllers
         [HttpPut("{cityId}/pointofinterest/{id}")]
         public IActionResult UpdatePointOfInterest(int cityId, int id, [FromBody] PointOfInterestForUpdateDto pointOfInterest)
         {
+            if (pointOfInterest == null)
+            {
+                return BadRequest();
+            }
 
+            if (pointOfInterest.Description == pointOfInterest.Name)
+            {
+                ModelState.AddModelError("Description", "The provided description should be different from the name.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterestFromStore = city.PointsOfInterest.FirstOrDefault(p =>
+            p.Id == id);
+
+            if (pointOfInterestFromStore == null)
+            {
+                return NotFound();
+            }
+
+            pointOfInterestFromStore.Name = pointOfInterest.Name;
+            pointOfInterestFromStore.Description = pointOfInterest.Description;
+
+            return NoContent();
         }
     }
 }
