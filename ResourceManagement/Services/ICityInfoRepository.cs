7828b3fb022cb8d7d8001e9d90b770ec2d025757
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-10-30 11:32:04 +0300

    ICityInfoRepository

---
+++ ResourceManagement/Services/ICityInfoRepository.cs
@@ -0,0 +1,20 @@
+﻿using ResourceManagement.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResourceManagement.Services
+{
+    public interface ICityInfoRepository
+    {
+        bool CityExists(int cityId);
+        IEnumerable<City> GetCities();
+        City GetCity(int cityId, bool includePointsOfInterest);
+        IEnumerable<PointOfInterest> GetPointsOfInterestForCity(int cityId);
+        PointOfInterest GetPointOfInterestForCity(int cityId, int pointOfInterestId);
+        void AddPointOfInterestForCity(int cityId, PointOfInterest pointOfInterest);
+        void DeletePointOfInterest(PointOfInterest pointOfInterest);
+        bool Save();
+    }
+}
