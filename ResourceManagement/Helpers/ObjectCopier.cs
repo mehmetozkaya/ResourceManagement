f3f6f4758cf1a0d50b180e955fa6181ca4154959
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-11-07 18:23:48 +0300

    Clone

--- ResourceManagement/Helpers/ObjectCopier.cs
+++ ResourceManagement/Helpers/ObjectCopier.cs
@@ -1,11 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace ResourceManagement.Helpers
 {
-    public class ObjectCopier
+    public static class ObjectCopier
     {
+        public static T Clone<T>(T source)
+        {
+            if (!typeof(T).IsSerializable)
+            {
+                throw new ArgumentException("The type must be serializable.", "source");
+            }
+
+            // Don't serialize a null object, simply return the default for that object
+            if (Object.ReferenceEquals(source, null))
+            {
+                return default(T);
+            }
+
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new MemoryStream();
+            using (stream)
+            {
+                formatter.Serialize(stream, source);
+                stream.Seek(0, SeekOrigin.Begin);
+                return (T)formatter.Deserialize(stream);
+            }
+        }
     }
 }
