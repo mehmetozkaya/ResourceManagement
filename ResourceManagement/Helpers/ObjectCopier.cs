7d6608ef7b8c79cb14b782bb1cc9c626fbd115bf
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-11-07 18:26:50 +0300

    SerializeHelper

--- ResourceManagement/Helpers/ObjectCopier.cs
+++ ResourceManagement/Helpers/ObjectCopier.cs
@@ -3,6 +3,9 @@ using System;
 using System.IO;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace ResourceManagement.Helpers
 {
@@ -67,6 +70,42 @@ namespace ResourceManagement.Helpers
         public object Clone()
         {
             throw new NotImplementedException();
+        }
+    }
+
+    public static class SerializeHelper
+    {
+        //Michael White, Holly Springs Consulting, 2009
+        //michael@hollyspringsconsulting.com
+        public static T DeserializeXML<T>(string xmlData) where T : new()
+        {
+            if (string.IsNullOrEmpty(xmlData))
+                return default(T);
+
+            TextReader tr = new StringReader(xmlData);
+            T DocItms = new T();
+            XmlSerializer xms = new XmlSerializer(DocItms.GetType());
+            DocItms = (T)xms.Deserialize(tr);
+
+            return DocItms == null ? default(T) : DocItms;
+        }
+
+        public static string SeralizeObjectToXML<T>(T xmlObject)
+        {
+            StringBuilder sbTR = new StringBuilder();
+            XmlSerializer xmsTR = new XmlSerializer(xmlObject.GetType());
+            XmlWriterSettings xwsTR = new XmlWriterSettings();
+
+            XmlWriter xmwTR = XmlWriter.Create(sbTR, xwsTR);
+            xmsTR.Serialize(xmwTR, xmlObject);
+
+            return sbTR.ToString();
+        }
+
+        public static T CloneObject<T>(T objClone) where T : new()
+        {
+            string GetString = SerializeHelper.SeralizeObjectToXML<T>(objClone);
+            return SerializeHelper.DeserializeXML<T>(GetString);
         }
     }
 }
