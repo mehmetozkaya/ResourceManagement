14dcf48297aa7b5009e6907fedce90140411dc2e
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-11-07 18:28:13 +0300

    CloneObject with reflection

--- ResourceManagement/Helpers/ObjectCopier.cs
+++ ResourceManagement/Helpers/ObjectCopier.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.IO;
+using System.Reflection;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
@@ -62,6 +63,49 @@ namespace ResourceManagement.Helpers
                     { pS.GetGetMethod().Invoke( S, null ) });
                 }
             };
+        }
+
+        public static object CloneObject(object opSource)
+        {
+            //grab the type and create a new instance of that type
+            Type opSourceType = opSource.GetType();
+            object opTarget = CreateInstanceOfType(opSourceType);
+
+            //grab the properties
+            PropertyInfo[] opPropertyInfo = opSourceType.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            //iterate over the properties and if it has a 'set' method assign it from the source TO the target
+            foreach (PropertyInfo item in opPropertyInfo)
+            {
+                if (item.CanWrite)
+                {
+                    //value types can simply be 'set'
+                    if (item.PropertyType.IsValueType || item.PropertyType.IsEnum || item.PropertyType.Equals(typeof(System.String)))
+                    {
+                        item.SetValue(opTarget, item.GetValue(opSource, null), null);
+                    }
+                    //object/complex types need to recursively call this method until the end of the tree is reached
+                    else
+                    {
+                        object opPropertyValue = item.GetValue(opSource, null);
+                        if (opPropertyValue == null)
+                        {
+                            item.SetValue(opTarget, null, null);
+                        }
+                        else
+                        {
+                            item.SetValue(opTarget, CloneObject(opPropertyValue), null);
+                        }
+                    }
+                }
+            }
+            //return the new item
+            return opTarget;
+        }
+
+        private static object CreateInstanceOfType(Type opSourceType)
+        {
+            throw new NotImplementedException();
         }
     }
 
