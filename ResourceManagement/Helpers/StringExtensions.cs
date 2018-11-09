0970dda6e5d1cb284fc06fed19e27ce6aaa8a305
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-11-09 21:06:09 +0300

    IDisposable

--- ResourceManagement/Helpers/StringExtensions.cs
+++ ResourceManagement/Helpers/StringExtensions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace ResourceManagement.Helpers
@@ -95,8 +96,40 @@ namespace ResourceManagement.Helpers
         }
     }
 
-    class SimpleCleanup
+    public class SimpleCleanup : IDisposable
     {
+        // some fields that require cleanup
+        private SafeHandle handle;
+        private bool disposed = false; // to detect redundant calls
 
+        public SimpleCleanup()
+        {
+            this.handle = null;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    // Dispose managed resources.
+                    if (handle != null)
+                    {
+                        handle.Dispose();
+                    }
+                }
+
+                // Dispose unmanaged managed resources.
+
+                disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
     }
 }
