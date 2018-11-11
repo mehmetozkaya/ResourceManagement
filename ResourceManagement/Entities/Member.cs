3de1069c20f5e4a022d7773bba23605260df8161
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-11-11 19:32:30 +0300

    Member

---
+++ ResourceManagement/Entities/Member.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResourceManagement.Entities
+{
+    public class Member
+    {
+        public int MemberID { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
