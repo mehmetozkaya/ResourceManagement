dc8de882584c66ed07aefe3550fa5910f5001a39
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-11-11 19:34:46 +0300

    MemberComment

--- ResourceManagement/Entities/Member.cs
+++ ResourceManagement/Entities/Member.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,5 +13,30 @@ namespace ResourceManagement.Entities
 
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public virtual ICollection<MemberComment> MemberComments { get; set; }
+
     }
+
+    public class Comment
+    {
+        public int CommentID { get; set; }
+        public string Message { get; set; }
+
+        public virtual ICollection<MemberComment> MemberComments { get; set; }
+    }
+
+    public class MemberComment
+    {
+        [Key, Column(Order = 0)]
+        public int MemberID { get; set; }
+        [Key, Column(Order = 1)]
+        public int CommentID { get; set; }
+
+        public virtual Member Member { get; set; }
+        public virtual Comment Comment { get; set; }
+
+        public int Something { get; set; }
+        public string SomethingElse { get; set; }
+    }
+
 }
